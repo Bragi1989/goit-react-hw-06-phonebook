a2f3e9dddab48c5ebfdee6c632aa1dcd0eb402b2
Bragi1989 <130233226+Bragi1989@users.noreply.github.com>
2023-11-27 15:01:37 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChangeFilter = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleSubmit = (name, number) => {
+    const normalizedContacts = contacts.map((contact) => ({
+      ...contact,
+      name: contact.name.toLowerCase(),
+    }));
+
+    const lowerCaseName = name.toLowerCase();
+
+    if (normalizedContacts.some((contact) => contact.name === lowerCaseName)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    setContacts((prevContacts) => [
+      ...prevContacts,
+      { id: Date.now(), name: lowerCaseName, number },
+    ]);
+  };
+
+  const handleDeleteContact = (id) => {
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm handleSubmit={handleSubmit} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleChangeFilter={handleChangeFilter} />
+      <ContactList contacts={filteredContacts} handleDeleteContact={handleDeleteContact} />
     </div>
   );
 };
+
+export default App;
